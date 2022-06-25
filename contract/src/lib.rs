5a470430a7c115618deb7c9fc3068d911e58ea80
Alex Vo <votanlean@gmail.com>
2022-06-25 08:12:05 +0700

    Add balance

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -9,64 +9,45 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{log, near_bindgen};
 
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
+const DEFAULT_BALANCE: u8 = 10;
 
-// Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    message: String,
+    balance: u8,
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract{
-    fn default() -> Self{
-        Self{message: DEFAULT_MESSAGE.to_string()}
+impl Default for Contract {
+    fn default() -> Self {
+        Self{balance: DEFAULT_BALANCE}
     }
 }
 
-// Implement the contract structure
 #[near_bindgen]
 impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
+    pub fn get_balance(&self) -> u8 {
+        return self.balance;
     }
-
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        // Use env::log to record logs permanently to the blockchain!
-        log!("Saving greeting {}", message);
-        self.message = message;
+    pub fn deposit(&mut self, amount: u8) {
+        log!("Deposit {} token", amount);
+        self.balance = self.balance + amount;
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn get_default_greeting() {
+    fn get_default_balance() {
         let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+        assert_eq!(contract.get_balance(), 10)
     }
 
     #[test]
-    fn set_then_get_greeting() {
+    fn deposit() {
         let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
+        contract.deposit(5);
+        assert_eq!(contract.get_balance(), 15);
     }
 }
